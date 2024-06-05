b28bf86b0d88e9ca8453e159b3517619c4877ab6
James-wasonga <jameswasonga22@gmail.com>
2024-06-05 23:58:53 +0300

    rust class

--- task6/src/area.rs
+++ task6/src/area.rs
@@ -1,9 +1,13 @@
 pub trait Area{
     fn area(&self) -> f64;
 }
+
+//struct for a circle
 pub struct Circle{
     pub radius: f64,
 }
+
+//implimenting the trait for the Circle
 
 impl Area for Circle {
     fn area(&self) -> f64{
@@ -12,10 +16,13 @@ impl Area for Circle {
 
 }
 
+// creating struct Rectangle
 pub struct Rectangle {
    pub length: f64,
    pub width: f64,
 }
+
+//implementing Area trait for Rectangle
 
 impl Area for Rectangle{
     fn area(&self) -> f64 {
