e67d25025d9a043a5be085fd8af74cac459b86c2
James-wasonga <jameswasonga22@gmail.com>
2024-06-27 18:48:46 +0300

    using handle to be prented threads in the order

--- practise_2/src/main.rs
+++ practise_2/src/main.rs
@@ -3,14 +3,14 @@ use std::time::Duration;
 
 fn main() {
     //creating the thread
-   thread::spawn(||{
+   let handle = thread::spawn(||{
     println!("Hello inner thread");
    });
-
+    handle.join().unwrap();
    println!("Hi main thread");
    //sleeping the main thread to print the inner thread
-  for i in 0..10 {
-    thread::sleep(Duration::from_millis(1));
- } 
+//   for i in 0..10 {
+//     thread::sleep(Duration::from_millis(1));
+//  } 
 
 }
