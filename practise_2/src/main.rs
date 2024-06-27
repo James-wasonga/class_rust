79776e19b25472276585d47550f090b662d654b3
James-wasonga <jameswasonga22@gmail.com>
2024-06-27 18:45:38 +0300

    creating a thread

--- practise_2/src/main.rs
+++ practise_2/src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    //creating the thread
+   thread::spawn(||{
+    println!("Hello inner thread");
+   });
+
+   println!("Hi main thread");
+   //sleeping the main thread to print the inner thread
+  for i in 0..10 {
+    thread::sleep(Duration::from_millis(1));
+ } 
+
 }
